d7024069682047caf2c942ecd294d7cee222df6f
Behruz22 <behruztuxtayev8@gmail.com>
2024-07-28 13:39:15 +0500

    There are changes

--- Buyurtmalar.cs
+++ Buyurtmalar.cs
@@ -5,26 +5,26 @@ namespace ExaminProject_2_modul;
 
 public class Buyurtmalar
 {
-    string buyurtmalar = Directory.GetCurrentDirectory()+"Buyurtmalar.json";
+    string buyurtmalar = Directory.GetCurrentDirectory() + "Buyurtmalar.txt";
     string menuPath = Directory.GetCurrentDirectory() + "Menyu.json";
     public void Buyurtma()
     {
         List<Dictionary<string, List<string>>> menu = new List<Dictionary<string, List<string>>>();
-        if(File.Exists(menuPath))
+        if (File.Exists(menuPath))
         {
-            using(StreamReader sr = new StreamReader(menuPath))
+            using (StreamReader sr = new StreamReader(menuPath))
             {
-                menu=JsonSerializer.Deserialize<List<Dictionary<string, List<string>>>>(sr.ReadToEnd());
+                menu = JsonSerializer.Deserialize<List<Dictionary<string, List<string>>>>(sr.ReadToEnd());
             }
             int i = 0;
-            foreach(var item in menu)
+            foreach (var item in menu)
             {
                 foreach (var key in item)
                 {
                     Console.Write($"{++i}. {key.Key}: ");
-                    foreach(var value in key.Value)
+                    foreach (var value in key.Value)
                     {
-                        Console.Write(value+" ");
+                        Console.Write(value + " ");
                     }
                     Console.WriteLine("\n");
                 }
@@ -35,25 +35,48 @@ public class Buyurtmalar
             Console.Write("Taomni tanlang: ");
             foreach (var item in menu[cat - 1])
             {
-                foreach(var key in item.Value)
+                foreach (var key in item.Value)
                 {
                     Console.WriteLine($"{++j}.{key}");
                 }
             }
-            int taomId=int.Parse(Console.ReadLine());
+            int taomId = int.Parse(Console.ReadLine());
             string newTaom;
             int n = 0;
+            string taom = string.Empty;
             foreach (var item in menu[cat - 1])
             {
                 foreach (var key in item.Value)
                 {
                     if (taomId == (++n))
                     {
-                        Console.WriteLine(key);
+                        taom = key;
                     }
                 }
             }
-            
+            string[] zakaz;
+            Console.Write("Ismingizni kiriting: ");
+            string name = Console.ReadLine();
+            if (File.Exists(buyurtmalar))
+            {
+                using (StreamReader sr = new StreamReader(buyurtmalar))
+                {
+                    zakaz = sr.ReadToEnd().Split("\n");
+                }
+                using (StreamWriter writer = new StreamWriter(buyurtmalar))
+                {
+                    writer.WriteLine(zakaz + $"Zakaz beruvchi: {name}, Buyrutmadagi taom: {taom}");
+                }
+            }
+            else
+            {
+                using (StreamWriter writer = new StreamWriter(buyurtmalar))
+                {
+                    writer.WriteLine($"Zakaz beruvchi: {name}, Buyrutmadagi taom: {taom}");
+                }
+            }
+            Console.WriteLine("Buyrutmangiz qabul qilindi!");
+
 
         }
         else
@@ -61,4 +84,24 @@ public class Buyurtmalar
             Console.WriteLine("Afsuski hali menyu mavjud emas!");
         }
     }
+
+    public void ListBuyurtma()
+    {
+        if (File.Exists(buyurtmalar))
+        {
+            using (StreamReader reader = new StreamReader(buyurtmalar))
+            {
+                foreach (var item in reader.ReadToEnd().Split("\n"))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Afsuski hali buyurtmalar mavjud emas!");
+        }
+    }
 }
+
+
--- Worker.cs
+++ Worker.cs
@@ -6,17 +6,17 @@ public class Worker
     public Worker()
     {
         Console.WriteLine("Restoranga xush kelibsiz!");
-        Malumot mal= new Malumot();
-        Kategoriyalar kat= new Kategoriyalar();
-        Buyurtmalar buyurtma= new Buyurtmalar();
+        Malumot mal = new Malumot();
+        Kategoriyalar kat = new Kategoriyalar();
+        Buyurtmalar buyurtma = new Buyurtmalar();
         Console.ReadKey();
         int cursor = 0;
-        List<string> restoran = new List<string>() { "Restoran admini","Mijoz","Chiqish"};
+        List<string> restoran = new List<string>() { "Restoran admini", "Mijoz", "Chiqish" };
         bool exit = true;
         while (exit)
         {
             Console.Clear();
-            for(int i = 0; i < restoran.Count; i++)
+            for (int i = 0; i < restoran.Count; i++)
             {
                 if (cursor == i)
                 {
@@ -27,44 +27,44 @@ public class Worker
                 Console.ResetColor();
             }
             var consol = Console.ReadKey();
-            if(consol.Key==ConsoleKey.DownArrow)
+            if (consol.Key == ConsoleKey.DownArrow)
             {
-                cursor=(cursor+1)%restoran.Count;
+                cursor = (cursor + 1) % restoran.Count;
             }
-            else if(consol.Key== ConsoleKey.UpArrow)
+            else if (consol.Key == ConsoleKey.UpArrow)
             {
-                cursor=(cursor+restoran.Count-1)%restoran.Count;
+                cursor = (cursor + restoran.Count - 1) % restoran.Count;
             }
-            else if(consol.Key==ConsoleKey.Enter)
+            else if (consol.Key == ConsoleKey.Enter)
             {
                 switch (cursor)
                 {
                     case 0:
                         int cursorAdmin = 0;
                         bool exitAdmin = true;
-                        List<string> Admin= new List<string>() {"Restoran haqida ma'lumot","Kategoriyalar","Menyu","Buyurtmalar","Orqaga"};
+                        List<string> Admin = new List<string>() { "Restoran haqida ma'lumot", "Kategoriyalar", "Menyu", "Buyurtmalar", "Orqaga" };
                         while (exitAdmin)
                         {
                             Console.Clear();
                             Console.WriteLine("Admin");
-                            for (int i = 0;i < Admin.Count; i++)
+                            for (int i = 0; i < Admin.Count; i++)
                             {
                                 if (cursorAdmin == i)
                                 {
-                                    Console.BackgroundColor= ConsoleColor.White;
+                                    Console.BackgroundColor = ConsoleColor.White;
                                     Console.ForegroundColor = ConsoleColor.Black;
                                 }
                                 Console.WriteLine(Admin[i]);
                                 Console.ResetColor();
                             }
-                            var consolAdmin= Console.ReadKey();
+                            var consolAdmin = Console.ReadKey();
                             if (consolAdmin.Key == ConsoleKey.DownArrow)
                             {
-                                cursorAdmin=(cursorAdmin+1)%Admin.Count;
+                                cursorAdmin = (cursorAdmin + 1) % Admin.Count;
                             }
-                            else if(consolAdmin.Key == ConsoleKey.UpArrow)
+                            else if (consolAdmin.Key == ConsoleKey.UpArrow)
                             {
-                                cursorAdmin=(cursorAdmin+Admin.Count-1)%Admin.Count;
+                                cursorAdmin = (cursorAdmin + Admin.Count - 1) % Admin.Count;
                             }
                             else if (consolAdmin.Key == ConsoleKey.Enter)
                             {
@@ -76,19 +76,19 @@ public class Worker
                                         List<string> malumot = new List<string>() { "Ma'lumot kiritish", "Ma'lumotni yangilash", "Ma'lumotni o'chirish", "Ma'lumotni ko'rish", "Orqaga" };
                                         while (exitMal)
                                         {
-                                            Console.Clear() ;
+                                            Console.Clear();
                                             Console.WriteLine("Admin");
-                                            for(int i = 0; i < malumot.Count; i++)
+                                            for (int i = 0; i < malumot.Count; i++)
                                             {
-                                                if(cursorMal == i)
+                                                if (cursorMal == i)
                                                 {
-                                                    Console.BackgroundColor= ConsoleColor.White;
+                                                    Console.BackgroundColor = ConsoleColor.White;
                                                     Console.ForegroundColor = ConsoleColor.Black;
                                                 }
                                                 Console.WriteLine(malumot[i]);
                                                 Console.ResetColor();
                                             }
-                                            var consolMal= Console.ReadKey();
+                                            var consolMal = Console.ReadKey();
                                             if (consolMal.Key == ConsoleKey.UpArrow)
                                             {
                                                 cursorMal = (cursorMal + Admin.Count - 1) % malumot.Count;
@@ -97,7 +97,7 @@ public class Worker
                                             {
                                                 cursorMal = (cursorMal + 1) % malumot.Count;
                                             }
-                                            else if(consolMal.Key== ConsoleKey.Enter)
+                                            else if (consolMal.Key == ConsoleKey.Enter)
                                             {
                                                 switch (cursorMal)
                                                 {
@@ -105,7 +105,7 @@ public class Worker
                                                         mal.WriteMal();
                                                         Console.ReadKey();
                                                         break;
-                                                     case 1:
+                                                    case 1:
                                                         mal.UpdateMal();
                                                         Console.ReadKey();
                                                         break;
@@ -118,7 +118,7 @@ public class Worker
                                                         Console.ReadKey();
                                                         break;
                                                     case 4:
-                                                        exitMal=false;
+                                                        exitMal = false;
                                                         break;
                                                 }
                                             }
@@ -127,31 +127,31 @@ public class Worker
                                     case 1:
                                         int cursorKat = 0;
                                         bool exitKat = true;
-                                        List<string> Kategoriya= new List<string>() {"Kategoriya yaratish","Kategoriyani o'zgartirish","Kategoriyani o'chirish","Kategoriya ro'yhati","Orqaga"};
+                                        List<string> Kategoriya = new List<string>() { "Kategoriya yaratish", "Kategoriyani o'zgartirish", "Kategoriyani o'chirish", "Kategoriya ro'yhati", "Orqaga" };
                                         while (exitKat)
                                         {
                                             Console.Clear();
                                             Console.WriteLine("Admin");
-                                            for(int i = 0;i < Kategoriya.Count; i++)
+                                            for (int i = 0; i < Kategoriya.Count; i++)
                                             {
                                                 if (cursorKat == i)
                                                 {
-                                                    Console.BackgroundColor= ConsoleColor.White;
-                                                    Console.ForegroundColor= ConsoleColor.Black;
+                                                    Console.BackgroundColor = ConsoleColor.White;
+                                                    Console.ForegroundColor = ConsoleColor.Black;
                                                 }
                                                 Console.WriteLine(Kategoriya[i]);
                                                 Console.ResetColor();
                                             }
-                                            var consolKat=Console.ReadKey();
+                                            var consolKat = Console.ReadKey();
                                             if (consolKat.Key == ConsoleKey.DownArrow)
                                             {
-                                                cursorKat=(cursorKat+1)%Kategoriya.Count;
+                                                cursorKat = (cursorKat + 1) % Kategoriya.Count;
                                             }
                                             else if (consolKat.Key == ConsoleKey.UpArrow)
                                             {
-                                                cursorKat=(cursorKat+Kategoriya.Count-1)%Kategoriya.Count;
+                                                cursorKat = (cursorKat + Kategoriya.Count - 1) % Kategoriya.Count;
                                             }
-                                            else if(consolKat.Key== ConsoleKey.Enter)
+                                            else if (consolKat.Key == ConsoleKey.Enter)
                                             {
                                                 Console.Clear();
                                                 switch (cursorKat)
@@ -185,7 +185,7 @@ public class Worker
                                         Console.ReadKey();
                                         break;
                                     case 3:
-                                        buyurtma.Buyurtma();
+                                        buyurtma.ListBuyurtma();
                                         Console.ReadKey();
 
                                         break;
@@ -198,32 +198,33 @@ public class Worker
                         break;
                     case 1:
                         int cursorMijoz = 0;
-                        bool exitMijoz=true;
-                        List<string> Mijoz = new List<string>() {"Restoran haqida ma'lumot","Buyurtma berish","Buyurtmalarim","Orqaga"};
+                        bool exitMijoz = true;
+                        List<string> Mijoz = new List<string>() { "Restoran haqida ma'lumot", "Buyurtma berish", "Buyurtmalarim", "Orqaga" };
                         while (exitMijoz)
                         {
                             Console.Clear();
-                            for(int i = 0; i < Mijoz.Count; i++)
+                            for (int i = 0; i < Mijoz.Count; i++)
                             {
                                 if (cursorMijoz == i)
                                 {
-                                    Console.BackgroundColor=ConsoleColor.White;
-                                    Console.ForegroundColor=ConsoleColor.Black;
+                                    Console.BackgroundColor = ConsoleColor.White;
+                                    Console.ForegroundColor = ConsoleColor.Black;
                                 }
                                 Console.WriteLine(Mijoz[i]);
                                 Console.ResetColor();
                             }
-                            var consolMijoz=Console.ReadKey();
+                            var consolMijoz = Console.ReadKey();
                             if (consolMijoz.Key == ConsoleKey.DownArrow)
                             {
-                                cursorMijoz=(cursorMijoz+1)%Mijoz.Count;
+                                cursorMijoz = (cursorMijoz + 1) % Mijoz.Count;
                             }
                             else if (consolMijoz.Key == ConsoleKey.UpArrow)
                             {
-                                cursorMijoz=(cursorMijoz+Mijoz.Count-1)%Mijoz.Count;
+                                cursorMijoz = (cursorMijoz + Mijoz.Count - 1) % Mijoz.Count;
                             }
-                            else if(consolMijoz.Key== ConsoleKey.Enter)
+                            else if (consolMijoz.Key == ConsoleKey.Enter)
                             {
+                                Console.Clear();
                                 switch (cursorMijoz)
                                 {
                                     case 0:
@@ -231,9 +232,12 @@ public class Worker
                                         Console.ReadKey();
                                         break;
                                     case 1:
-
+                                        buyurtma.Buyurtma();
+                                        Console.ReadKey();
                                         break;
                                     case 2:
+                                        buyurtma.ListBuyurtma();
+                                        Console.ReadKey();
                                         break;
                                     case 3:
                                         exitMijoz = false;
@@ -245,7 +249,7 @@ public class Worker
 
                         break;
                     case 2:
-                        exit= false;
+                        exit = false;
                         break;
                 }
             }
